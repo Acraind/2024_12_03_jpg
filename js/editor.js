d23eb4f564224d4ce306bc8f8a5435ef1323bd5e
Acraind <frederic.ren.zou@mail.com>
2024-12-05 14:19:23 +0100

    injection des params sur le loader

--- js/editor.js
+++ js/editor.js
@@ -1,4 +1,9 @@
+function loadEditor(params) {
+  console.log(params);
+  loadEditorEvent();
+}
 function loadEditorEvent() {
+  console.log(router.currentRoute());
   document.forms["editor-form"].addEventListener("submit", function (evt) {
     evt.preventDefault();
     console.log("form submit");
